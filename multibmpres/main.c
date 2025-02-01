8aa6aa7fb7d2bb7371ca71a8b2fa9c79e7eda97e
noahw2023 <noah.wooten@icloud.com>
2025-01-31 23:45:36 -0800

    v1.0 working

--- multibmpres/main.c
+++ multibmpres/main.c
@@ -8,6 +8,7 @@
 #define __crt_strcmp strcmp
 #define __crt_memcpy memcpy
 #define __crt_strcpy strcpy
+#define __crt_memset memset
 
 typedef struct _RESOURCE_ITEM {
 	char ResName[256];
@@ -29,20 +30,125 @@ void* MultiBmpClient_GetBmpDataFromRes(void* ResList, int i, unsigned short* Wid
 void* MultiBmpClient_AddResWithBmpData(void* ResList, unsigned long* ResListSize, char* Name, void* BmpFileData);
 void* MultiBmpClient_Compress(void* RawIn, unsigned long RawSize, unsigned long* OutSize);
 void* MultiBmpClient_Decompress(void* CompIn, unsigned long CompSize, unsigned long* UnCompSize);
+char* MultiBmpClient_GetResName(void* _ResList, int i);
+unsigned long MultiBmpClient_GetResSize(void* _ResList, int i, unsigned long* Width, unsigned long* Height);
 unsigned char* MultiBmpClient_GetBytesFromBMP(unsigned char* fileData, int* width, int* height, int* bytesPerPixel, unsigned long* RawDataSize);
 
+
 int main(int argc, char** argv) {
+	if (argc == 1) {
+		// interactive mode
+
+		FILE* File = fopen("res.bin", "rb");
+		if (!File) {
+			File = fopen("res.bin", "wb");
+			
+			RESOURCE_LIST ResList = { 0 };
+			ResList.FirstRes = 0x4;
+			fwrite(&ResList, sizeof(RESOURCE_LIST), 1, File);
+			RESOURCE_ITEM FirstItem = { 0 };
+			fwrite(&FirstItem, sizeof(RESOURCE_ITEM), 1, File);
+			fclose(File);
+		}
+
+		File = fopen("res.bin", "rb+");
+		if (!File) {
+			printf("Failed to open file.\n");
+			return -1;
+		}
+
+		unsigned long ResListSize;
+		fseek(File, 0, SEEK_END);
+		ResListSize = ftell(File);
+		fseek(File, 0, SEEK_SET);
+
+		void* _ResList = malloc(ResListSize);
+		fread(_ResList, ResListSize, 1, File);
+
+		printf("MultiBMPRes Tool v1.00\n");
+		printf("(c) Noah Wooten, All Rights Reserved 2023-2025\n\n");
+
+		while (1) {
+			printf("1.) Add new resource\n");
+			printf("2.) List resources\n");
+			printf("3.) Exit\n");
+
+			char Inbuffer[64];
+			fgets(Inbuffer, 64, stdin);
+			int Selector = atoi(Inbuffer);
+
+			switch (Selector) {
+			case 1: // add new resource
+				printf("Resource file name: ");
+				fgets(Inbuffer, 128, stdin);
+				if (strstr(Inbuffer, "\n"))
+					strstr(Inbuffer, "\n")[0] = 0x00;
+
+				FILE* _Open = fopen(Inbuffer, "rb");
+				if (!_Open) {
+					printf("Failed to open destination file.\n");
+					return 0;
+				}
+				
+				unsigned long OpenData;
+				fseek(_Open, 0, SEEK_END);
+				OpenData = ftell(_Open);
+				fseek(_Open, 0, SEEK_SET);
+
+				void* Data = malloc(OpenData);
+				fread(Data, OpenData, 1, _Open);
+				fclose(_Open);
+
+				printf("Resource name: ");
+					fgets(Inbuffer, 128, stdin);
+				if (strstr(Inbuffer, "\n"))
+					strstr(Inbuffer, "\n")[0] = 0x00;
+
+				unsigned long OldResListSize = ResListSize;
+				_ResList = MultiBmpClient_AddResWithBmpData(_ResList, &ResListSize, Inbuffer, Data);
+				free(Data);
+
+				printf("Added resource '%s', added %i KiB. (%i%% compression)\n", Inbuffer, (ResListSize - OldResListSize) / 1024, 100 - (((ResListSize - OldResListSize) / OpenData) * 100));
+				break;
+			case 2: // list resources
+				int ResCnt = MultiBmpClient_GetResCount(_ResList);
+				for (int i = 0; i < ResCnt; i++) {
+					unsigned long Width, Height;
+					unsigned long Compressed = MultiBmpClient_GetResSize(_ResList, i, &Width, &Height) / 1024;
+					unsigned long FullSize = (Width * Height * 3) / 1024;
+					printf("%i.) '%s' (%ix%i, %i KiB, %i KiB Uncompressed)\n", i + 1, MultiBmpClient_GetResName(_ResList, i),
+						Width, Height, Compressed, FullSize);
+				}
+				break;
+			default: // exit
+			case 3:
+				fseek(File, 0, SEEK_SET);
+				fwrite(_ResList, ResListSize, 1, File);
+				printf("Wrote %i KiB.\n", ResListSize / 1024);
+				fclose(File);
+				return 0;
+				break;
+			}
+
+		}
+	} else {
+		// scripting mode
+	}
+
 	return 0;
 }
 
 int MultiBmpClient_GetResCount(void* _ResList) {
 	PRESOURCE_LIST ResList = _ResList;
 	unsigned long Count = 0;
-	PRESOURCE_ITEM Item = (char*)_ResList + ResList->FirstRes;
+	PRESOURCE_ITEM Item = ((char*)_ResList) + ResList->FirstRes;
 	
 	do {
 		Count++;
-		Item = (char*)_ResList + Item->NextResource;
+		if (Item->NextResource && Item->NextResource != 0xFFFFFFFF)
+			Item = (char*)_ResList + Item->NextResource;
+		else
+			break;
 	} while (Item->NextResource);
 
 	return Count;
@@ -78,7 +184,8 @@ void* MultiBmpClient_GetBmpDataFromRes(void* _ResList, int i, unsigned short* Wi
 		}
 
 		Count++;
-		Item = (char*)_ResList + Item->NextResource;
+		if (Item->NextResource)
+			Item = (char*)_ResList + Item->NextResource;
 	} while (Item->NextResource);
 
 	return NULL;
@@ -88,28 +195,68 @@ void* MultiBmpClient_AddResWithBmpData(void* _ResList, unsigned long* ResListSiz
 	PRESOURCE_LIST ResList = _ResList;
 	PRESOURCE_ITEM Item = (char*)_ResList + ResList->FirstRes;
 
-	do {
-		if (!Item->NextResource) {
-			_ResList = __crt_realloc(_ResList, *ResListSize + sizeof(RESOURCE_ITEM));
-			PRESOURCE_ITEM NewItem = (char*)_ResList + *ResListSize;
-			*ResListSize += sizeof(RESOURCE_ITEM);
+	unsigned long OldNext = ResList->FirstRes;
 
+	do {
+		if (!Item->RawDataLocation) {
 			unsigned long Width, Height, BPP, RawSize;
 			void* BitmapData = MultiBmpClient_GetBytesFromBMP(BmpFileData, &Width, &Height, &BPP, &RawSize);
 
 			_ResList = __crt_realloc(_ResList, *ResListSize + RawSize);
 			__crt_memcpy((char*)_ResList + *ResListSize, BitmapData, RawSize);
+			PRESOURCE_ITEM NewItem = (char*)_ResList + OldNext;
 
 			NewItem->Width = Width;
 			NewItem->Height = Height;
 			NewItem->RawDataLocation = *ResListSize;
 			NewItem->RawDataSize = RawSize;
 			__crt_strcpy(NewItem->ResName, Name);
-			NewItem->NextResource = 0;
-
 			*ResListSize += RawSize;
-		}
+			NewItem->NextResource = *ResListSize;
+			
+			_ResList = __crt_realloc(_ResList, *ResListSize + sizeof(RESOURCE_ITEM));
+			__crt_memset((char*)_ResList + *ResListSize, 0, sizeof(RESOURCE_ITEM));
 
+			*ResListSize += sizeof(RESOURCE_ITEM);
+
+			return _ResList;
+		} else {
+			OldNext = Item->NextResource;
+			Item = (char*)_ResList + Item->NextResource;
+		}
+	} while (1);
+}
+
+char* MultiBmpClient_GetResName(void* _ResList, int i) {
+	PRESOURCE_LIST ResList = _ResList;
+	unsigned long Count = 0;
+	PRESOURCE_ITEM Item = (char*)_ResList + ResList->FirstRes;
+
+	do {
+		if (Count == i) {
+			return Item->ResName;
+		}
+		Count++;
 		Item = (char*)_ResList + Item->NextResource;
 	} while (Item->NextResource);
+
+	return NULL;
+}
+
+unsigned long MultiBmpClient_GetResSize(void* _ResList, int i, unsigned long* Width, unsigned long* Height) {
+	PRESOURCE_LIST ResList = _ResList;
+	unsigned long Count = 0;
+	PRESOURCE_ITEM Item = (char*)_ResList + ResList->FirstRes;
+
+	do {
+		if (Count == i) {
+			*Width = Item->Width;
+			*Height = Item->Height;
+			return Item->RawDataSize;
+		}
+		Count++;
+		Item = (char*)_ResList + Item->NextResource;
+	} while (Item->NextResource);
+
+	return NULL;
 }
